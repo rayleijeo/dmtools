f11c2c0f0549f287b75f9a86242bfaecf789e05c
zhangshuanglei <zhangshuanglei@163.com>
2015-08-05 07:31:03 +0800

    小修改

---
+++ DmTools/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing; 
+using System.Text;
+using System.Windows.Forms;
+
+namespace DmTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdobpersonal_CheckedChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void rdobcompany_CheckedChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
