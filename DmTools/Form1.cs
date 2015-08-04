e9ab4c928c8215b559d7c72a01c9f3392e13a45a
zhangshuanglei <zhangshuanglei@163.com>
2015-08-04 08:09:16 +0800

    first

---
+++ DmTools/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing; 
+using System.Text;
+using System.Windows.Forms;
+
+namespace DmTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdobpersonal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdobpersonal.Checked) {
+                gpbcompany.Visible = false;
+                gpbpersonal.Visible = true;
+            }
+        }
+
+        private void rdobcompany_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdobcompany.Checked)
+            {
+                gpbcompany.Visible = true;
+                gpbpersonal.Visible = false;
+            }
+        }
+    }
+}
